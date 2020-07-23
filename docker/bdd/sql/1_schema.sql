f81ce5c386af49ef9da7830641896fbb1bdb4d40
vianney diris <vianney.diris@gmail.com>
2020-07-23 09:55:36 +0200

    fix mpd and schema

--- docker/bdd/sql/1_schema.sql
+++ docker/bdd/sql/1_schema.sql
@@ -1,45 +1,69 @@
 
-CREATE SEQUENCE author_id_seq;
+CREATE SEQUENCE public.serie_id_seq;
 
-CREATE TABLE author (
-                id BIGINT NOT NULL DEFAULT nextval('author_id_seq'),
+CREATE TABLE public.serie (
+                id BIGINT NOT NULL DEFAULT nextval('public.serie_id_seq'),
+                name VARCHAR NOT NULL,
+                CONSTRAINT serie_pk PRIMARY KEY (id)
+);
+
+
+ALTER SEQUENCE public.serie_id_seq OWNED BY public.serie.id;
+
+CREATE SEQUENCE public.status_id_seq;
+
+CREATE TABLE public.status (
+                id BIGINT NOT NULL DEFAULT nextval('public.status_id_seq'),
+                name VARCHAR NOT NULL,
+                CONSTRAINT status_pk PRIMARY KEY (id)
+);
+
+
+ALTER SEQUENCE public.status_id_seq OWNED BY public.status.id;
+
+CREATE SEQUENCE public.author_id_seq;
+
+CREATE TABLE public.author (
+                id BIGINT NOT NULL DEFAULT nextval('public.author_id_seq'),
                 name VARCHAR NOT NULL,
                 CONSTRAINT author_pk PRIMARY KEY (id)
 );
 
 
-ALTER SEQUENCE author_id_seq OWNED BY author.id;
+ALTER SEQUENCE public.author_id_seq OWNED BY public.author.id;
 
-CREATE SEQUENCE type_id_seq;
+CREATE SEQUENCE public.type_id_seq;
 
-CREATE TABLE type (
-                id BIGINT NOT NULL DEFAULT nextval('type_id_seq'),
+CREATE TABLE public.type (
+                id BIGINT NOT NULL DEFAULT nextval('public.type_id_seq'),
                 name VARCHAR NOT NULL,
                 CONSTRAINT type_pk PRIMARY KEY (id)
 );
 
 
-ALTER SEQUENCE type_id_seq OWNED BY type.id;
+ALTER SEQUENCE public.type_id_seq OWNED BY public.type.id;
 
-CREATE TABLE book (
+CREATE TABLE public.book (
                 id BIGINT NOT NULL,
                 title VARCHAR NOT NULL,
                 type_id BIGINT NOT NULL,
                 image_path VARCHAR NOT NULL,
                 resume VARCHAR NOT NULL,
                 author_id BIGINT NOT NULL,
+                status_id BIGINT NOT NULL,
+                serie_id BIGINT NOT NULL,
                 CONSTRAINT book_pk PRIMARY KEY (id)
 );
 
 
-CREATE TABLE console (
+CREATE TABLE public.console (
                 id BIGINT NOT NULL,
                 name VARCHAR NOT NULL,
                 CONSTRAINT console_pk PRIMARY KEY (id)
 );
 
 
-CREATE TABLE game (
+CREATE TABLE public.game (
                 id BIGINT NOT NULL,
                 title VARCHAR NOT NULL,
                 console_id BIGINT NOT NULL,
@@ -49,23 +73,37 @@ CREATE TABLE game (
 );
 
 
-ALTER TABLE book ADD CONSTRAINT author_book_fk
+ALTER TABLE public.book ADD CONSTRAINT serie_book_fk
+FOREIGN KEY (serie_id)
+REFERENCES public.serie (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.book ADD CONSTRAINT status_book_fk
+FOREIGN KEY (status_id)
+REFERENCES public.status (id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.book ADD CONSTRAINT author_book_fk
 FOREIGN KEY (author_id)
-REFERENCES author (id)
+REFERENCES public.author (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE book ADD CONSTRAINT type_book_fk
+ALTER TABLE public.book ADD CONSTRAINT type_book_fk
 FOREIGN KEY (type_id)
-REFERENCES type (id)
+REFERENCES public.type (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
-ALTER TABLE game ADD CONSTRAINT console_game_fk
+ALTER TABLE public.game ADD CONSTRAINT console_game_fk
 FOREIGN KEY (console_id)
-REFERENCES console (id)
+REFERENCES public.console (id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
